bb42e137116636b7e444c61ef496cd12c7feb332
Cby <jorisgonc@hotmail.fr>
2020-04-06 13:13:54 +0200

    LampeTorche

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject torchLight;
+
+    public static GameManager s_Singleton;
+
+    private void Awake()
+    {
+        if (s_Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_Singleton = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            torchLight.GetComponent<LampeTorche>().ToggleTorchLightAppear();
+        }
+    }
+}
