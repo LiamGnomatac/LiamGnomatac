a09ee7f4ad120a203825c9b315296819afbde0d3
Cby <jorisgonc@hotmail.fr>
2020-04-06 13:16:25 +0200

    Modif GM

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,9 +28,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
-        {
-            torchLight.GetComponent<LampeTorche>().ToggleTorchLightAppear();
-        }
+        
     }
 }
