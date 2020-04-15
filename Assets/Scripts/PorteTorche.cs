92abd6bab2ee96c1ca34b78c49e088465ef8d16b
Cby <jorisgonc@hotmail.fr>
2020-04-15 17:06:25 +0200

    porte-torche enigme 1 fini

--- Assets/Scripts/PorteTorche.cs
+++ Assets/Scripts/PorteTorche.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PorteTorche : MonoBehaviour
 {
+    public Collider torchE;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(torchE == null)
+        {
+            Debug.Log("Torche à renseigner dans porteTorche");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != torchE)
+        { return; }
+
+        if(other == torchE)
+        {
+            GameManager.s_Singleton.firstEIsComplete = true;
+            torchE.transform.position = transform.position;
+            torchE.transform.Rotate(transform.rotation.eulerAngles);
+            torchE.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            Debug.Log("Enigme complété");
+        }
     }
 }
