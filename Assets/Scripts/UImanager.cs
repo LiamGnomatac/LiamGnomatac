ebeb22e22ad4ee359b2601ea1a287561f8b718a8
LiamGnomatac <liam.rogron@e-artsup.net>
2020-04-20 11:45:49 +0200

    Tp Cassé à réparer

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UImanager : MonoBehaviour
+public class UiManager : MonoBehaviour
 {
+    public Image transitionTp;
+
+    public static UiManager s_Singleton;
+
+    private void Awake()
+    {
+        if (s_Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_Singleton = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transitionTp.canvasRenderer.SetAlpha(0);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void FadeIn()
     {
-        
+        transitionTp.CrossFadeAlpha(1, 1, false);
     }
+
+    public void FadeOut()
+    {
+        transitionTp.CrossFadeAlpha(0, 1, false);
+    }
+
 }
