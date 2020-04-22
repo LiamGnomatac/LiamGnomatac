979a49dece7b1a446c70c10d4256acb999da09c2
Destati <joanna.ehrhardt@e-artsup.net>
2020-04-22 15:09:28 +0100

    Ajouts d'assets models 3D.

---
+++ Assets/Scripts/TasDePierre.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TasDePierre : MonoBehaviour
+{
+    int triggerManette = 0;
+    public GameObject TasDePierres;
+    public GameObject PierreALancer;
+    public Material defaultMaterial;
+    public Material newMaterialTrigger;
+    public bool TasDePierresIsTriggerOrNot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerManette > 0)
+        {
+            TasDePierresIsTriggerOrNot = true;
+        }
+
+        if (triggerManette <= 0)
+        {
+            TasDePierresIsTriggerOrNot = false;
+        }
+
+        if (triggerManette > 0 && Input.GetKeyDown("s"))
+        {
+            Instantiate(PierreALancer);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            triggerManette++;
+            TasDePierres.GetComponent<Renderer>().material = newMaterialTrigger;
+            Debug.Log("IsTrigger dans la manette!");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            triggerManette--;
+            TasDePierres.GetComponent<Renderer>().material = defaultMaterial;
+        }
+    }
+
+    }
