9f39fb3eb8fd7dadabea51eb47d374e6a6b2da67
LiamGnomatac <liam.rogron@e-artsup.net>
2020-04-22 15:00:00 +0200

    FadeIn et FadeOut Ok, à modifier pour rallonger la durée

--- Assets/Scripts/TPPerso.cs
+++ Assets/Scripts/TPPerso.cs
@@ -27,8 +27,12 @@ public class TPPerso : MonoBehaviour
     private GameObject vue;
     private GameObject select;
 
+    public float _fadeDuration = 5f;
+
     void Start()
     {
+        FadeFromBlack();
+       
         player = Player.instance;
         fwd = cameraVR.TransformDirection(Vector3.forward);
        
@@ -91,13 +95,13 @@ public class TPPerso : MonoBehaviour
 
                 if (teleport.stateUp)
                 {
-
+                    FadeToBlack();
                     // TP Center                
                     center.transform.position = hit.transform.position;
                     Debug.Log("GetFlashTrue");
                     Destroy(select);
                     zoneSelectActive = false;
-                    
+                    FadeFromBlack();
                 }
             }
             
@@ -124,6 +128,22 @@ public class TPPerso : MonoBehaviour
         
     }
 
-    
-   
+    private void FadeToBlack()
+    {
+        //set start color
+        SteamVR_Fade.Start(Color.clear, 0f);
+        //set and start fade to
+        SteamVR_Fade.Start(Color.black, _fadeDuration);
+    }
+
+
+
+    private void FadeFromBlack()
+    {
+        //set start color
+        SteamVR_Fade.Start(Color.black, 0f);
+        //set and start fade to
+        SteamVR_Fade.Start(Color.clear, _fadeDuration);
+    }
+
 }
