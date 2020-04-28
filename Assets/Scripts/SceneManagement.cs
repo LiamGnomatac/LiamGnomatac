ffe9441683ea004a925973f135cd3556f605c0da
LiamGnomatac <liam.rogron@e-artsup.net>
2020-04-28 10:36:32 +0200

    Canvas Fonctionnel

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    int buildIndex;
+    public static SceneManagement s_Singleton;
+
+    private void Awake()
+    {
+        if (s_Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_Singleton = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GetCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        buildIndex = currentScene.buildIndex;
+    }
+
+    public void LoadPreviewScene()
+	{
+        switch (buildIndex)
+        {
+            case 0:
+                // Do something...
+                SceneManager.LoadScene(buildIndex);
+                break;
+            case 1:
+                // Do something...
+                SceneManager.LoadScene(buildIndex);
+                break;
+        }
+    }
+
+}
