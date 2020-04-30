dcd8262894f5aff4db3b20d27b20898c92532868
Destati <joanna.ehrhardt@e-artsup.net>
2020-04-30 16:49:29 +0100

    Correction de script.75% de finition.

--- Assets/Scripts/Sac.cs
+++ Assets/Scripts/Sac.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Sac : MonoBehaviour
 {
+
     public GameObject sac ;
     public GameObject porte ;
-    public GameObject telephone ;
-    public GameObject lampe ;
+    public GameObject telephone;
+    public GameObject lampeTorche;
     public Rigidbody rigidbodyPorte ;
     public bool CanOpenDoor ; 
     float numberOfEssentialsObjects;
     int trigger = 0 ;
 
+
+
     // Start is called before the first frame update
     void Start()
     {
         numberOfEssentialsObjects = 0;
+        
     }
 
     // Update is called once per frame
@@ -37,7 +41,7 @@ public class Sac : MonoBehaviour
         if (numberOfEssentialsObjects == 2)
         {
             CanOpenDoor = true;
-            Debug.Log("il y a 2/2 objet important dans le sac");
+            Debug.Log("il y a 2/2 objet important dans le sac, la porte peux s'ouvrir");
         }
 
         if (CanOpenDoor == true)
@@ -50,6 +54,8 @@ public class Sac : MonoBehaviour
             rigidbodyPorte.constraints = RigidbodyConstraints.FreezePositionY;
             rigidbodyPorte.constraints = RigidbodyConstraints.FreezeRotationY;
         }
+
+       
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -58,8 +64,26 @@ public class Sac : MonoBehaviour
             trigger++;
             numberOfEssentialsObjects += 1;
             Debug.Log("Un Objet important à été mis dans le sac");
+
+        }
+
+        if (other.gameObject.tag == "Telephone")
+        {
+            trigger++;
+            telephone.SetActive(false);
+            numberOfEssentialsObjects += 1;
+            Debug.Log("Le téléphone a été mis dans le sac et ne peux plus etre récupérer");
+        }
+
+        if (other.gameObject.tag == "LampeTorche")
+        {
+            trigger++;
+            lampeTorche.SetActive(false);
+            numberOfEssentialsObjects += 1;
+            Debug.Log("La Lampe torche a été mis dans le sac et ne peux plus etre récupérer");
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Essential")
