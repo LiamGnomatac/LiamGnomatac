33b8bb5010f86b7175c8e59c1a1dd31d09888533
LiamGnomatac <liam.rogron@e-artsup.net>
2020-05-02 01:42:24 +0200

    Scorpion fonctionnel à 90 %

--- Assets/Scripts/Script Monstre/ScorpionScript.cs
+++ Assets/Scripts/Script Monstre/ScorpionScript.cs
@@ -8,20 +8,76 @@ public class ScorpionScript : MonoBehaviour
     public GameObject zone1;
     public GameObject zone2;
     public GameObject zone3;
-
+    public float compteurMur = 8f;
+    public float compteurPlafond = 15f;
+    public float timerNextTP = 20f;
+    public bool scorpionMove = false;
+    public bool scorpionMur = false;
+    public bool scorpionHitFlash = false;
+    public bool scorpionPlafond = false;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScorpionMove();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(scorpionMove == true)
+        {
+
+            timerNextTP -= Time.deltaTime;
+           
+        }
+
+       if (scorpionPlafond == true)
+        {
+            compteurPlafond -= Time.deltaTime;
+            
+        }
+
+       if(scorpionMur == true)
+        {
+
+            compteurMur -= Time.deltaTime;
+            
+
+        }
+
+       if (scorpionHitFlash == true && scorpionMur == true)
+        {
+
+            scorpionMur = false;
+            Debug.Log("Le scorpion a fui");
+            ScorpionMove();
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (timerNextTP <= 0 && scorpionMove == true)
+        {
+            scorpionMove = false;
+            ScorpionTP();
+        }
+
+        if (compteurPlafond <= 0 && scorpionPlafond == true)
+        {
+            scorpionPlafond = false;
+            ScorpionMove();
+        }
+
+        if(compteurMur <= 0)
+        {
+            KillingScorpion();
+        }
+
     }
 
     public void ScorpionTP()
@@ -35,7 +91,7 @@ public class ScorpionScript : MonoBehaviour
             case 0:
 
                 gameObject.transform.position = zone1.transform.position;
-                
+                ScorpionMur();
                 
 
 
@@ -43,24 +99,54 @@ public class ScorpionScript : MonoBehaviour
             case 1:
 
                 gameObject.transform.position = zone2.transform.position;
-                
-             
+                ScorpionMur();
+
 
                 break;
 
             case 2:
 
                 gameObject.transform.position = zone3.transform.position;
-               
-                
+                ScorpionPlaflond();
+
 
                 break;
         }
     }
 
+    public void ScorpionMove()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        int tempsSup = Random.Range(0, 21);
+        timerNextTP = 20 + tempsSup;
+        scorpionMove = true;
+        Debug.Log("Le scorpion se déplace");
+
+
+
+    }
+
+    public void ScorpionMur()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        compteurMur = 8f;
+        scorpionMur = true;
+        Debug.Log("Le scorpion est dans un mur");
+
+    }
+
+    public void ScorpionPlaflond()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        compteurPlafond = 15f;
+        scorpionPlafond = true;
+        Debug.Log("Le scorpion est dans une cavité au plafond");
+
+    }
+
     public void KillingScorpion()
     {
-        Debug.Log("Joueur tué par le chien");
+        Debug.Log("Joueur tué par le scorpion");
         
         SceneManagement.s_Singleton.GetKilled();
     }
