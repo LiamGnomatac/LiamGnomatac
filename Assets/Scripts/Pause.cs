44598d916fa668ed591c4286c4e649d0ac91e16a
Cby <jorisgonc@hotmail.fr>
2020-05-04 14:00:23 +0200

    Débutpause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    private bool isPause;
+
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            TogglePause();
+            ToggleCanvas();
+        }
+    }
+
+    public void ToggleCanvas()
+    {
+        canvas.SetActive(!canvas.activeSelf);
+    }
+    
+    public void TogglePause()
+    {
+        if(isPause)
+        {
+            Time.timeScale = 1;
+            isPause = false;
+            ToggleCanvas();
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPause = true;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
