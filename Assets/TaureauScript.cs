00d6fc68fc012a86a2419392d83e9d6b11d65715
LiamGnomatac <liam.rogron@e-artsup.net>
2020-05-06 00:38:59 +0200

    NAVMESH DE SES MORTS

--- Assets/TaureauScript.cs
+++ Assets/TaureauScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TaureauScript : MonoBehaviour
 {
+
+    public float vitesse = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
