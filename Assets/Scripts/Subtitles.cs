9ac352d69d5fbc2eac32118a8c894118bc1dfb8a
Destati <joanna.ehrhardt@e-artsup.net>
2020-05-06 10:28:10 +0100

    Tout les boutons de l'ui marche sauf l'augmentation et la diminution du volume sonore dans les options. Raccourcis claviers: p = pause ; o= options ; m= écran de mort ; s= sous titre

---
+++ Assets/Scripts/Subtitles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Subtitles : MonoBehaviour
+{
+    public List<GameObject> listeDesLignesDeSousTitres = new List<GameObject> { };
+    public int uneLigneDeSousTitre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in listeDesLignesDeSousTitres)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
--- Assets/Scripts/UiSimulator.cs
+++ Assets/Scripts/UiSimulator.cs
@@ -1,17 +1,143 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+
 
 public class UiSimulator : MonoBehaviour
 {
+    public GameObject UiDeLaMort;
+    public GameObject UiDeLaPause;
+    public GameObject UiDeLesOptions;
+    public GameObject UiDeLesSousTitres;
+
+    public Button BoutonMortOui;
+    public Button BoutonMortNon;
+    public Button BoutonMortQuitter;
+
+    public Button BoutonPauseReprendre;
+    public Button BoutonPauseNon;
+    public Button BoutonPauseQuitter;
+
+    public Button BoutonOptionsVolume;
+    public Button BoutonOptionsPlus;
+    public Button BoutonOptionsMoins;
+    public Button BoutonOptionsSousTitres;
+    public Button BoutonOptionsOui;
+    public Button BoutonOptionsNon;
+    public Button BoutonOptionsRetour;
+
+    public bool mort;
+
+    public AudioMixer audioMixer;
+
+    public void EffetDesBoutonsRelancerLaSceneActive()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void EffetDesBoutonsAllerAuBureau()
+    {
+        SceneManager.LoadScene("BureauAsset");
+    }
+
+    public void EffetDesBoutonsQuitterLeJeu()
+    {
+        Application.Quit(); 
+    }
+
+    public void EffetDesBoutonsReprendre()
+    {
+        UiDeLaPause.SetActive(false);
+    }
+
+    public void EffetDesBoutonsRetour()
+    {
+        UiDeLesOptions.SetActive(false);
+    }
+
+    public void EffetDuBoutonOuiDesSousTitres()
+    {
+        UiDeLesSousTitres.SetActive(true); 
+    }
+
+    public void EffetDuBoutonNonDesSousTitres()
+    {
+        UiDeLesSousTitres.SetActive(false);
+    }
+
+    public void LeSonPlusFort(float volume=100)
+    {
+        AudioListener.volume =+ 10;
+    }
+
+    public void LeSonMoinsFort(float volume=100)
+    {
+        AudioListener.volume =- 10;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UiDeLaMort.SetActive(false);
+        UiDeLaPause.SetActive(false);
+        UiDeLesOptions.SetActive(false);
+        UiDeLesSousTitres.SetActive(false);
+        mort = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            UiDeLaPause.SetActive(true);
+            UiDeLaMort.SetActive(!UiDeLaMort);
+            UiDeLesOptions.SetActive(!UiDeLesOptions);
+            UiDeLesSousTitres.SetActive(!UiDeLesSousTitres);
+            mort = false;
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            UiDeLesOptions.SetActive(true);
+            UiDeLaMort.SetActive(!UiDeLaMort);
+            UiDeLaPause.SetActive(!UiDeLaPause);
+            UiDeLesSousTitres.SetActive(!UiDeLesSousTitres);
+            mort = false;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            UiDeLesSousTitres.SetActive(true);
+            UiDeLaMort.SetActive(!UiDeLaMort);
+            UiDeLaPause.SetActive(!UiDeLaPause);
+            UiDeLesOptions.SetActive(!UiDeLesOptions);
+            mort = false;
+        }
+
+        if (Input.GetKeyDown("m"))
+        {
+            mort = !mort;
+            UiDeLaPause.SetActive(!UiDeLaPause);
+            UiDeLesOptions.SetActive(!UiDeLesOptions);
+            UiDeLesSousTitres.SetActive(!UiDeLesSousTitres);
+        }
+
+        if (mort == true)
+        {
+            UiDeLaMort.SetActive(true);
+            
+        }
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
     {
         
     }
