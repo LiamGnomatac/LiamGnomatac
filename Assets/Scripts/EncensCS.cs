6d2c87a95c044e28e63bbb2e7d2ae6cbb60f5b0e
Cby <jorisgonc@hotmail.fr>
2020-05-07 14:29:54 +0200

    Encens fonctionnel et ménage

---
+++ Assets/Scripts/EncensCS.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncensCS : MonoBehaviour
+{
+    public bool isTurnOn;
+    public bool isFilled;
+    public float stayOnTimer = 0;
+    public GameObject oil;
+    public GameObject vfxFlame;
+    public GameObject pointLight;
+
+    private bool isStayOn;
+    private float lightIntensity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightIntensity = pointLight.GetComponent<Light>().intensity;
+        StartTurnOn();
+        StayOn();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isStayOn)
+        {
+            return;
+        }
+        else if (isStayOn && GetComponentInChildren<Light>())
+        {
+            Debug.Log("va s'eteindre");
+            Invoke("TurnOffWithTime", stayOnTimer);
+        }
+    }
+
+    private void FilledWithOil()
+    {
+        isFilled = true;
+        oil.SetActive(true);
+        Debug.Log("GlouGlouGlou");
+    }
+
+    private void StartTurnOn()
+    {
+        if(isTurnOn)
+        {
+            FilledWithOil();
+            TurnOn();
+        }
+    }
+
+    private void TurnOn()
+    {
+        if(isFilled)
+        {
+            Debug.Log("Flamme");
+            isTurnOn = true;
+            pointLight.SetActive(true);
+            pointLight.GetComponent<Light>().intensity = lightIntensity;
+            Instantiate(vfxFlame, pointLight.transform);
+        }
+    }
+
+    public void TurnOff()
+    {
+        Debug.Log("Plus de Flamme");
+        isTurnOn = false;
+        pointLight.SetActive(false);
+        Destroy(pointLight.GetComponentInChildren<ParticleSystem>().gameObject);
+    }
+
+    private void RandTurnOn()
+    {
+        Debug.Log("clong");
+        if(!isTurnOn)
+        {
+            int rand = Random.Range(0, 3);
+            if (rand != 0)
+            {
+                return;
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
+        
+    }
+
+    private void TurnOffWithTime()
+    {
+        pointLight.GetComponent<Light>().intensity -= 0.05f;
+        if(pointLight.GetComponent<Light>().intensity <= 0)
+        {
+            TurnOff();
+        }
+    }
+
+    private void StayOn()
+    {
+        if(stayOnTimer != 0)
+        {
+            isStayOn = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<PierreAFeuCS>())
+        {
+            if (isFilled)
+            {
+                if (other.gameObject.GetComponent<PierreAFeuCS>().NumberTimeCollision() >= 1)
+                {
+                    RandTurnOn();
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/PierreAFeuCS.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class PierreAFeuCS : MonoBehaviour
+{
+    private int collisionNumber;
+    public int NumberTimeToFall;
+    public GameObject vfxSpark;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(collisionNumber);
+    }
+
+    public int NumberTimeCollision()
+    {
+        return collisionNumber;
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Etincelle")
+        {
+            Debug.Log("Tappe un autre silex");
+            collisionNumber++;
+            Instantiate(vfxSpark, transform.position, Quaternion.identity);
+        }
+        if (collisionNumber >= NumberTimeToFall)
+        {
+            Debug.Log("tombe");
+            Destroy(GetComponent<Throwable>());
+            collisionNumber = 0;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        gameObject.AddComponent<Throwable>();
+    }
+
+}
