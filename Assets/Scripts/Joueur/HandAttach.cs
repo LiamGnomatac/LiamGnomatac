db78f1c917d7b641a9178f3330bcb0cbeaa12dc4
Cby <jorisgonc@hotmail.fr>
2020-05-10 13:43:42 +0200

    Test tas de caillou

---
+++ Assets/Scripts/Joueur/HandAttach.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class HandAttach : MonoBehaviour
+    {
+        private Hand hand;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            hand = GetComponent<Hand>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SetAttachObj(GameObject GObj)
+        {
+            hand.AttachObject(GObj, GrabTypes.Scripted, Hand.AttachmentFlags.ParentToHand);
+        }
+    }
+}
---
+++ Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+ public class RockSpawner : MonoBehaviour
+ {
+    public GameObject rock;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private GameObject InvokeRock()
+        {
+            GameObject newrock = Instantiate(rock, transform.position, Quaternion.identity);
+            return newrock;
+        }
+
+        /*private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<Hand>())
+            {
+                Debug.Log("normalement ça marche");
+                other.gameObject.GetComponent<HandAttach>().SetAttachObj(InvokeRock());
+            }
+        }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Hand>())
+        {
+            Debug.Log("normalement ça marche");
+            collision.gameObject.GetComponent<HandAttach>().SetAttachObj(InvokeRock());
+        }
+    }
+
+
+}
