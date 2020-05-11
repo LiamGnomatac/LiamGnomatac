77195b001ea8115ac21ccf1eae877d7f4219415c
Cby <jorisgonc@hotmail.fr>
2020-05-11 11:58:38 +0200

    ça marche !

---
+++ Assets/Scripts/AssignFuctionToButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class AssignFuctionToButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        HoverButton b = gameObject.GetComponent<HoverButton>();
+        b.onButtonDown.AddListener(delegate { SceneManagement.s_Singleton.LoadPreviewScene(); });
+    }
+
+}
