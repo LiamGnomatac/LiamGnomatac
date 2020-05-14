814e8382118ea231b0ec48f0bfafe74ab786aa23
Cby <jorisgonc@hotmail.fr>
2020-05-14 22:36:39 +0200

    point Meta JOANNA !!! + manager

--- Assets/Scripts/BruitManager.cs
+++ Assets/Scripts/BruitManager.cs
@@ -24,7 +24,7 @@ public class BruitManager : MonoBehaviour
 
 
         // Start is called before the first frame update
-        void Start()
+    void Start()
     {
         bruit.SetActive(false);
     }
