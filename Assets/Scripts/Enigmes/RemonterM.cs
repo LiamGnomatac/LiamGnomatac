41c1e78a04d388cc6397385ff95136075ad77755
Cby <jorisgonc@hotmail.fr>
2020-06-16 12:04:12 +0200

    remonter script

--- Assets/Scripts/Enigmes/RemonterM.cs
+++ Assets/Scripts/Enigmes/RemonterM.cs
@@ -6,8 +6,45 @@ public class RemonterM : MonoBehaviour
 {
     public float timeBeforeTimerDeathBegin = 5;
     public float countdownBeforeDeath = 30;
+    public GameObject zoneFin;
+    public GameObject VFXFlame;
+    public List<Transform> spawnPointFlame;
+    public StoryElementMonologue monologue;
     private void Start()
     {
-        
+        zoneFin.SetActive(false);
+        Debug.Log("false");
+        Invoke("FirstTimerBegin", timeBeforeTimerDeathBegin);
+    }
+
+    private void FirstTimerBegin()
+    {
+        Invoke("CountdownDeathBegin", countdownBeforeDeath);
+    }
+
+    private void CountdownDeathBegin()
+    {
+        ScreamerManager.s_Singleton.KillingScorpion();
+    }
+
+    public void GoToCredits()
+    {
+        Debug.Log("crédit");
+        zoneFin.SetActive(true);
+    }
+
+    public void spawnFlames()
+    {
+        Debug.Log("bcp flames");
+        for (int i = 0; i < spawnPointFlame.Count; i++)
+        {
+            Instantiate(VFXFlame, spawnPointFlame[i]);
+        }
+        if(monologue)
+        {
+            monologue.TriggerMonologue();
+            Destroy(monologue);
+        }
+        GoToCredits();
     }
 }
---
+++ Assets/Scripts/Enigmes/ReworkScriptsEnigmes/InstanceFlames.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class InstanceFlames : MonoBehaviour
+{
+    public GameObject flameVFX;
+    private bool canMakeFlame;
+    private int numberOfFlames;
+    public StoryElementMonologue monologue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GetComponent<Interactable>().attachedToHand != null)
+        {
+            canMakeFlame = true;
+            if(collision.gameObject.layer == 9)
+            {
+                Instantiate(flameVFX, transform.position, Quaternion.identity);
+                numberOfFlames++;
+                if(numberOfFlames > 5)
+                {
+                    FindObjectOfType<RemonterM>().GoToCredits();
+                    monologue.TriggerMonologue();
+                    GetComponentInChildren<Light>().gameObject.SetActive(false);
+                    Destroy(monologue);
+                    Destroy(this);
+                }
+            }
+        }
+        if(canMakeFlame && collision.gameObject.layer == 9)
+        {
+            FindObjectOfType<RemonterM>().spawnFlames();
+        }
+    }
+}
